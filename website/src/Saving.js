390e3635d82d42e57d684fd713521d706debc10b
Nico Beulich <beulichn@yahoo.de>
2022-02-08 08:13:22 +0100

    added files for new saving feature

--- website/src/Saving.js
+++ website/src/Saving.js
@@ -2,7 +2,9 @@
 //Imports
 import './App.css';
 import Nav from './Nav';
-
+import TabScreen from './components/Saving/TabScreen';
+import InputArea from './components/Saving/InputArea';
+import Total from './components/Saving/Total'
 
 //--------------------------------------------------------------------------------------------------------------------
 //Rendering
@@ -10,6 +12,20 @@ function Finance() {
     return (
         <>
             <Nav />
+            <div id="window">
+                <div className="windowObject">
+                    <Total />
+                </div>
+                <div className="windowObject">
+                    <InputArea />
+                </div>
+                <div className="windowObject">
+                    <TabScreen />
+                </div>
+                <div className="windowObject">
+                    
+                </div>
+            </div>
         </>
 
     );
